ac7722337978fa943ab0c1e2136fad16c3993045
Nutchapon Hanouypornlert <nutchaponhanouypornlert@Nutchapons-MacBook-Pro.local>
2024-02-17 13:08:08 +0700

    setup docker for postgres database

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,12 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-const env = process.env
-const PORT = env.PORT || 3000
+const env = process.env;
+const PORT = env.PORT || 3000;
 
 async function bootstrap() {
-
   const app = await NestFactory.create(AppModule);
+
   await app.listen(PORT);
 }
 bootstrap();
