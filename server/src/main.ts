9888396a5ed3629e0ce1726e1fba2982ce6e255c
Nutchapon Hanouypornlert <nutchaponhanouypornlert@Nutchapons-MacBook-Pro.local>
2024-02-17 11:26:48 +0700

    setup docker for server

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const env = process.env
+const PORT = env.PORT || 3000
+
 async function bootstrap() {
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(PORT);
 }
 bootstrap();
